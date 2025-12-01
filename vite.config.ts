cc3bd5030d67fde894c267862bf8ad71a6b96942
kodai motonishi <kodai820820@icloud.com>
2025-12-01 13:51:39 +0900

    fix(build): Explicitly pass config to tailwindcss plugin

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from 'tailwindcss';
 import autoprefixer from 'autoprefixer';
+import tailwindConfig from './tailwind.config.js';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,7 +10,7 @@ export default defineConfig({
   css: {
     postcss: {
       plugins: [
-        tailwindcss,
+        tailwindcss(tailwindConfig),
         autoprefixer,
       ],
     },
